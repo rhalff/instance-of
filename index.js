42112fa47eea75c55f81ce5d7f83b9bce4041bb7
Rob Halff <rob.halff@gmail.com>
2020-02-08 21:28:33 +0100

    fix case where toString has space between function and parenthesis

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function InstanceOf(obj, type) {
   if(t === 'object') {
     if(type.toLowerCase() === t) return true; // Object === object
     if(obj.constructor.name === type) return true;
-    if(obj.constructor.toString().match(/function (\w*)/)[1] === type) return true;
+    if(obj.constructor.toString().match(/function\s*(\w*)/)[1] === type) return true;
     return InstanceOf(Object.getPrototypeOf(obj), type);
   } else {
     return t === type;
